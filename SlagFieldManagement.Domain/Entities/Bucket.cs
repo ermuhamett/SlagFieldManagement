59cacc6820e91ed0083b5f64b77bcb086dee6f8d
Ermuhamet <ermyhamet2002@gmail.com>
2025-03-31 19:53:40 +0500

    Создано сущность справочника ковша

---
+++ SlagFieldManagement.Domain/Entities/Bucket.cs
@@ -0,0 +1,21 @@
+﻿namespace SlagFieldManagement.Domain.Entities;
+
+public sealed class Bucket
+{
+    public Guid Id { get; private set; }
+    public string Description { get; private set; }
+    public bool IsDelete { get; private set; }
+    
+    private Bucket(Guid id, string description)
+    {
+        Id = id;
+        Description = description;
+        IsDelete = false;
+    }
+
+    public static Bucket Create(string description)
+    {
+        var bucket = new Bucket(Guid.NewGuid(), description);
+        return bucket;
+    }
+}
