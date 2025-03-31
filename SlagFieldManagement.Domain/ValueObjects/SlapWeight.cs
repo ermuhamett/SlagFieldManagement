236282517622757e7f61a679000793a3be7ab41d
Ermuhamet <ermyhamet2002@gmail.com>
2025-03-31 19:24:03 +0500

    Добавлен VO для SlapWeight

---
+++ SlagFieldManagement.Domain/ValueObjects/SlapWeight.cs
@@ -0,0 +1,9 @@
+﻿namespace SlagFieldManagement.Domain.ValueObjects;
+
+public record SlapWeight(decimal Value)
+{
+    public static SlapWeight Zero => new(0);
+    public static SlapWeight FromKilograms(decimal kilograms) => new(kilograms);
+
+    public override string ToString() => $"{Value} kg";
+}
