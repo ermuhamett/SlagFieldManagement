793da447d645df24632f0375b003905fc4a8e3a5
Ermuhamet <ermyhamet2002@gmail.com>
2025-03-31 19:19:46 +0500

    Добавлено первоначальные интерфейсы для домена

---
+++ SlagFieldManagement.Domain/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,9 @@
+﻿namespace SlagFieldManagement.Domain.Interfaces;
+
+public interface IAggregateRoot
+{
+    int Id { get; }
+    void LoadFromHistory(IEnumerable<IDomainEvent> events);
+    IEnumerable<IDomainEvent> GetUncommittedEvents();
+    void ClearUncommittedEvents();
+}
---
+++ SlagFieldManagement.Domain/Interfaces/IDomainEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace SlagFieldManagement.Domain.Interfaces;
+
+public interface IDomainEvent
+{
+    int EventId { get; }
+    int AggregateId { get; }
+    string EventType { get; }
+    string EventData { get; }
+    int Version { get; }
+    DateTime Timestamp { get; }
+    string Metadata { get; }
+}
