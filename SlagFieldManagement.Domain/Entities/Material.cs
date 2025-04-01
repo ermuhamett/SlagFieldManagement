817dabf53ba4d733c1b4a856e0fd0f10357839e9
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-01 11:35:59 +0500

    Создано первичная версия сущности материал

---
+++ SlagFieldManagement.Domain/Entities/Material.cs
@@ -0,0 +1,21 @@
+﻿namespace SlagFieldManagement.Domain.Entities;
+
+public sealed class Material
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public bool IsDelete { get; private set; }
+
+    private Material(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+        IsDelete = false;
+    }
+
+    public static Material Create(string name)
+    {
+        var material = new Material(Guid.NewGuid(), name);
+        return material;
+    }
+}
