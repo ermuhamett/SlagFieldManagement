07fad505df1b4af82a540694d7f803a58221f815
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-03 23:42:41 +0500

    Убрано лишний value object

--- SlagFieldManagement.Domain/ValueObjects/SlagWeight.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SlagFieldManagement.Domain.ValueObjects;
-
-public record SlagWeight(decimal Value)
-{
-    public static SlagWeight Zero => new(0);
-    public static SlagWeight FromKilograms(decimal kilograms) => new(kilograms);
-
-    public override string ToString() => $"{Value} kg";
-}
