d5820723f1b7fbddf53aec87c3b41710457d0e3c
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-08 21:59:33 +0500

    Добавлено абстрактный интерфейс для работы с бд

---
+++ SlagFieldManagement.Domain/Abstractions/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace SlagFieldManagement.Domain.Abstractions;
+
+public interface IRepository<T> where T:Entity
+{
+    Task<T?> GetByIdAsync(Guid id);
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+}
