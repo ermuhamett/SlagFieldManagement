a4d6400f915b5f008562c2a240631ef773a604ec
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-10 22:25:20 +0500

    Обновленный класс Error представляющий null ошибки

--- SlagFieldManagement.Domain/Abstractions/Error.cs
+++ SlagFieldManagement.Domain/Abstractions/Error.cs
@@ -2,5 +2,8 @@
 
 public record Error(string Code, string Message)
 {
+    // Статическое поле, представляющее отсутствие ошибки.
     public static Error None => new(string.Empty, string.Empty);
+    // Статическое поле, представляющее ошибку, связанную с null значением.
+    public static Error NullValue = new("Error.NullValue", "Null value was provided");
 }
