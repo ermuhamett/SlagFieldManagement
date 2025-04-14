5ee2a6d6efb3b623b02444057ab049669580504d
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-14 22:36:53 +0500

    Создано интерфейс агрегатов

---
+++ SlagFieldManagement.Domain/Aggregates/SlagFieldPlace/ISlagFieldPlaceRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace SlagFieldManagement.Domain.Aggregates.SlagFieldPlace;
+
+public interface ISlagFieldPlaceRepository
+{
+    Task<SlagFieldPlace?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task AddAsync(SlagFieldPlace place, CancellationToken cancellationToken = default);
+    Task UpdateAsync(SlagFieldPlace place, CancellationToken cancellationToken = default);
+
+    Task<List<SlagFieldPlace>> GetAllAsync(CancellationToken cancellationToken = default);
+}
---
+++ SlagFieldManagement.Domain/Aggregates/SlagFieldState/ISlagFieldStateRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace SlagFieldManagement.Domain.Aggregates.SlagFieldState;
+
+public interface ISlagFieldStateRepository
+{
+    Task<SlagFieldState?> GetActiveStateAsync(Guid placeId, CancellationToken cancellationToken = default);
+    Task AddAsync(SlagFieldState state, CancellationToken cancellationToken = default);
+    Task UpdateAsync(SlagFieldState state, CancellationToken cancellationToken = default);
+}
