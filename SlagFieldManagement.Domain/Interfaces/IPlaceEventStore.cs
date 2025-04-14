ccafcbe4d6a2ac6d114a5c58921ac9d3a8b43244
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-14 22:39:50 +0500

    Изменено интерфейс хранилищ событий

--- SlagFieldManagement.Domain/Interfaces/IPlaceEventStore.cs
+++ SlagFieldManagement.Domain/Interfaces/IPlaceEventStore.cs
@@ -4,4 +4,12 @@ public interface IPlaceEventStore
 {
     Task SaveEventAsync(IDomainEvent @event, Guid placeId, int expectedVersion);
     Task<IEnumerable<IDomainEvent>> GetEventsForPlaceAsync(Guid placeId);
+    Task<List<IDomainEvent>> GetEventsBeforeAsync(Guid placeId, DateTime time, CancellationToken cancellationToken = default);
+    
+    // Новый метод для пакетной загрузки
+    Task<List<IDomainEvent>> GetEventsBeforeForPlacesAsync(
+        List<Guid> placeIds, 
+        DateTime snapshotTime, 
+        CancellationToken cancellationToken = default
+    );
 }
--- SlagFieldManagement.Domain/Interfaces/IStateEventStore.cs
+++ SlagFieldManagement.Domain/Interfaces/IStateEventStore.cs
@@ -3,5 +3,12 @@
 public interface IStateEventStore
 {
     Task SaveEventAsync(IDomainEvent @event, Guid stateId, int expectedVersion);
-    Task<IEnumerable<IDomainEvent>> GetEventsForStateAsync(Guid stateId);
+    Task<IEnumerable<IDomainEvent>> GetEventsBeforeAsync(Guid stateId, DateTime time, CancellationToken cancellationToken=default);
+    
+    // Новый метод для пакетной загрузки
+    Task<List<IDomainEvent>> GetEventsBeforeForPlacesAsync(
+        List<Guid> placeIds, 
+        DateTime snapshotTime, 
+        CancellationToken cancellationToken = default
+    );
 }
