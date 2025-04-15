4712e2b35287d92ec04148610414a4276597cd6a
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-15 15:33:28 +0500

    DTO класс используется для ответа в запросах

---
+++ SlagFieldManagement.Application/DTO/SlagFieldStateResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace SlagFieldManagement.Application.DTO;
+
+public sealed record SlagFieldStateResponse
+{
+    public Guid PlaceId { get; init; }
+    public string Row { get; init; }
+    public int Number { get; init; }
+    public bool IsEnable { get; init; }
+    public Guid? StateId { get; init; }
+    public string? State { get; init; } // BucketPlaced, BucketEmptied или "NotInUse" для неактивных мест
+    public Guid? BucketId { get; init; }
+    public Guid? MaterialId { get; init; }
+    public decimal? SlagWeight { get; init; }
+    public DateTime? StartDate { get; init; }
+    public DateTime? EndDate { get; init; }
+    public string? Description { get; init; }
+}
