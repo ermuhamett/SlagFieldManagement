db64595b8c3c38e00dd10c848a71758b1670260e
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-15 15:35:24 +0500

    Добавлено класс ошибок для шлакового поле и FluentValidation для валидации входящих данных

---
+++ SlagFieldManagement.Domain/Exceptions/SlagFieldErrors.cs
@@ -0,0 +1,10 @@
+﻿using SlagFieldManagement.Domain.Abstractions;
+
+namespace SlagFieldManagement.Domain.Exceptions;
+
+public static class SlagFieldErrors
+{
+    public static Error BucketNotFound(Guid bucketId) => new(
+        "SlagFieldErrors.BucketNotFound",
+        $"Ковш {bucketId} не найдено");
+}
