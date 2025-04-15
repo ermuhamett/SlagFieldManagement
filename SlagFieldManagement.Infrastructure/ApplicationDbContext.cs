53d8e5ca2fc2e78b1fcc83753c9811302925aee0
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-15 17:33:26 +0500

    Создано первоначальная форма DbContext и DI класса

---
+++ SlagFieldManagement.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SlagFieldManagement.Domain.Abstractions;
+
+namespace SlagFieldManagement.Infrastructure;
+
+public sealed class ApplicationDbContext:DbContext, IUnitOfWork
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Автоматическое применение всех конфигураций сущностей (IEntityTypeConfiguration) из текущей сборки.
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+    public async Task BeginTransactionAsync(CancellationToken ct = default)
+    {
+        await Database.BeginTransactionAsync(ct);
+    }
+
+    public async Task CommitTransactionAsync(CancellationToken ct = default)
+    {
+        await Database.CommitTransactionAsync(ct);
+    }
+
+    public async Task RollbackTransactionAsync(CancellationToken ct = default)
+    {
+        await Database.RollbackTransactionAsync(ct);
+    }
+}
--- SlagFieldManagement.Infrastructure/DependencyInjection.cs
+++ SlagFieldManagement.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,14 @@
-﻿namespace SlagFieldManagement.Infrastructure;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SlagFieldManagement.Infrastructure;
 
 public static class DependencyInjection
 {
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services;
+    }
 }
