3932b145ae75af3d65537fc6cf1eb9809d2270c3
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-15 15:34:01 +0500

    Репозиторий ковша и склада для слоя приложение

---
+++ SlagFieldManagement.Domain/Interfaces/IBucketRepository.cs
@@ -0,0 +1,9 @@
+﻿using SlagFieldManagement.Domain.Entities;
+
+namespace SlagFieldManagement.Domain.Interfaces;
+
+public interface IBucketRepository
+{
+    Task<Bucket?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task AddAsync(Bucket bucket, CancellationToken ct = default);
+}
---
+++ SlagFieldManagement.Domain/Interfaces/ISlagFieldStockRepository.cs
@@ -0,0 +1,26 @@
+﻿using SlagFieldManagement.Domain.Entities;
+
+namespace SlagFieldManagement.Domain.Interfaces;
+
+public interface ISlagFieldStockRepository
+{
+    /// <summary>
+    /// Получает запись SlagFieldStock по её уникальному идентификатору.
+    /// </summary>
+    Task<SlagFieldStock?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Получает запись SlagFieldStock по идентификатору состояния SlagFieldStateId.
+    /// </summary>
+    Task<SlagFieldStock?> GetByStateIdAsync(Guid stateId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Добавляет новую запись SlagFieldStock в хранилище.
+    /// </summary>
+    Task AddAsync(SlagFieldStock stock, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Обновляет существующую запись SlagFieldStock в хранилище.
+    /// </summary>
+    Task UpdateAsync(SlagFieldStock stock, CancellationToken cancellationToken = default);
+}
