27f40c6358d03a3b0a41b289c46b875f3c341202
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-17 17:18:12 +0500

    Созданы доменные сущности пользователь и роль

---
+++ SlagFieldManagement.Domain/Entities/Role.cs
@@ -0,0 +1,30 @@
+﻿using SlagFieldManagement.Domain.Abstractions;
+
+namespace SlagFieldManagement.Domain.Entities;
+
+public sealed class Role:Entity
+{
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    
+    // Приватный конструктор для EF Core
+    private Role() { }
+    
+    // Фабричный метод
+    public static Result<Role> Create(string name, string description)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            return Result.Failure<Role>(new Error("Role.EmptyName", "Название роли не может быть пустым."));
+
+        var role = new Role
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            Description = description ?? string.Empty,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        return Result.Success(role);
+    }
+}
---
+++ SlagFieldManagement.Domain/Entities/User.cs
@@ -0,0 +1,52 @@
+﻿using SlagFieldManagement.Domain.Abstractions;
+
+namespace SlagFieldManagement.Domain.Entities;
+
+public class User:Entity
+{
+    public Guid RoleId { get; private set; }
+    public string UserName { get; private set; }
+    public string PasswordHash { get; private set; }
+    public string Email { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? UpdatedAt { get; private set; }
+    
+    // Навигационное свойство
+    public Role Role { get; private set; }
+    
+    private User() { }
+    
+    // Фабричный метод
+    public static Result<User> Create(
+        Guid roleId,
+        string userName,
+        string passwordHash,
+        string email)
+    {
+        if (string.IsNullOrWhiteSpace(userName))
+            return Result.Failure<User>(new Error("User.EmptyUsername", "Логин не может быть пустым."));
+
+        if (string.IsNullOrWhiteSpace(email))
+            return Result.Failure<User>(new Error("User.EmptyEmail", "Email не может быть пустым."));
+
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            RoleId = roleId,
+            UserName = userName,
+            PasswordHash = passwordHash,
+            Email = email,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = null
+        };
+
+        return Result.Success(user);
+    }
+
+    // Метод для обновления данных
+    public void Update(string email, DateTime updatedAt)
+    {
+        Email = email;
+        UpdatedAt = updatedAt;
+    }
+}
