d37d640cc9297c852457deaf09aefe59621eb61b
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-26 22:06:05 +0500

    Реализовано репозиторий для работы с материалами

---
+++ SlagFieldManagement.Domain/Interfaces/IMaterialRepository.cs
@@ -0,0 +1,12 @@
+﻿using SlagFieldManagement.Domain.Entities;
+
+namespace SlagFieldManagement.Domain.Interfaces;
+
+public interface IMaterialRepository
+{
+    Task<Material?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task<List<Material>> GetAllAsync(CancellationToken ct = default);
+    Task AddAsync(Material material, CancellationToken ct = default);
+    void Update(Material material);
+    void Delete(Material material);
+}
---
+++ SlagFieldManagement.Infrastructure/Repositories/MaterialRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SlagFieldManagement.Domain.Entities;
+using SlagFieldManagement.Domain.Interfaces;
+
+namespace SlagFieldManagement.Infrastructure.Repositories;
+
+internal sealed class MaterialRepository:Repository<Material>, IMaterialRepository
+{
+    public MaterialRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+
+    public async Task<List<Material>> GetAllAsync(CancellationToken ct = default)
+    {
+        return await DbContext.Set<Material>()
+            .Where(m => !m.IsDelete)
+            .ToListAsync(ct);
+    }
+}
