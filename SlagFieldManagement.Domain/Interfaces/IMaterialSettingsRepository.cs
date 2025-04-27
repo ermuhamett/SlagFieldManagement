b6b89000c18a8f98bbfdc8eddf18f0fef0531016
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-27 15:01:32 +0500

    Реализовано репозиторий для MaterialSettings сущности

---
+++ SlagFieldManagement.Domain/Interfaces/IMaterialSettingsRepository.cs
@@ -0,0 +1,11 @@
+﻿using SlagFieldManagement.Domain.Entities;
+
+namespace SlagFieldManagement.Domain.Interfaces;
+
+public interface IMaterialSettingsRepository
+{
+    Task<MaterialSettings?> GetByMaterialIdAsync(Guid materialId, CancellationToken ct = default);
+    Task<List<MaterialSettings>> GetAllMaterials(CancellationToken ct = default);
+    Task AddAsync(MaterialSettings settings, CancellationToken ct = default);
+    void Update(MaterialSettings settings);
+}
---
+++ SlagFieldManagement.Infrastructure/Repositories/MaterialSettingsRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SlagFieldManagement.Domain.Entities;
+using SlagFieldManagement.Domain.Interfaces;
+
+namespace SlagFieldManagement.Infrastructure.Repositories;
+
+internal sealed class MaterialSettingsRepository:Repository<MaterialSettings>, IMaterialSettingsRepository
+{
+    public MaterialSettingsRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+
+    public async Task<MaterialSettings?> GetByMaterialIdAsync(
+        Guid materialId, 
+        CancellationToken ct = default)
+    {
+        return await DbContext.Set<MaterialSettings>()
+            .FirstOrDefaultAsync(s => s.MaterialId == materialId && !s.IsDelete, ct);
+    }
+
+    public async Task<List<MaterialSettings>> GetAllMaterials(CancellationToken ct = default)
+    {
+        return await DbContext.Set<MaterialSettings>()
+            .Where(ms => !ms.IsDelete)
+            .ToListAsync(ct);
+    }
+}
