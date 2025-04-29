c20105a9f6d65df9ead066eebd3ebfd5d03e345c
Ermuhamet <ermyhamet2002@gmail.com>
2025-04-29 20:25:36 +0500

    Удален свойство MaterialSettings

--- SlagFieldManagement.Domain/Entities/Material.cs
+++ SlagFieldManagement.Domain/Entities/Material.cs
@@ -6,18 +6,12 @@ public sealed class Material:Entity
 {
     public string Name { get; private set; }
     public bool IsDelete { get; private set; }
-    public MaterialSettings? Settings { get; private set; } // Ссылка на настройки
     
-    
-    private Material(Guid id, string name, MaterialSettings? settings):base(id)
+    private Material(Guid id, string name):base(id)
     {
         Name = name;
-        Settings = settings;
         IsDelete = false;
     }
 
-    public static Material Create(string name, MaterialSettings settings)
-    {
-        return new Material(Guid.NewGuid(), name, settings);
-    }
+    public static Material Create(string name) => new Material(Guid.NewGuid(), name);
 }
